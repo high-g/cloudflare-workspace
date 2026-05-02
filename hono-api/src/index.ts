a343fbef4beaf7dc0dc7eeae985d1202c074fba5
fumiya <altema1217bbfumi@live.jp>
2026-05-03 03:09:19 +0900

    fmt

--- hono-api/src/index.ts
+++ hono-api/src/index.ts
@@ -1,8 +1,8 @@
-import {Hono} from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => c.json({message: 'Hello from Workers!'}))
-app.get('/posts', (c) => c.json({posts: []}))
+app.get("/", (c) => c.json({ message: "Hello from Workers!" }));
+app.get("/posts", (c) => c.json({ posts: [] }));
 
-export default app
+export default app;
